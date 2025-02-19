79d2af87cd64ce4d3e8836f0f4d31b3d35547fdf
Alexdlmo <alexdelasmorenas10@gmail.com>
2025-02-19 08:45:40 +0100

    Estructura Organizativa del proyecto:Clase Instanciables y Estáticas

---
+++ EjemploPrueba/InterfazUsuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPrueba
+{
+    /// <summary>
+    /// Clase Estática que proporciona los métodos que posibilitan la interacción con el usuario
+    /// </summary>
+    public static class InterfazUsuario
+    {
+
+    }
+}
---
+++ EjemploPrueba/Vehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploPrueba
+{
+    /// <summary>
+    /// Clase Instanciable - Se puede crear objetos que se alojarán en memoria
+    /// </summary>
+    public class Vehiculo
+    {
+
+    }
+}
