28220b2c4b2797e969d9de41095377f13f8bb4e6
Alexdlmo <alexdelasmorenas10@gmail.com>
2025-02-19 09:07:40 +0100

    Establecimiento de la Estructura del Modelo de Datos (Clase Instanciable)

--- EjemploPrueba/Vehiculo.cs
+++ EjemploPrueba/Vehiculo.cs
@@ -11,6 +11,26 @@ namespace EjemploPrueba
     /// </summary>
     public class Vehiculo
     {
+        #region CONSTANTES - PARÁMETROS DE LA CONFIGURACIÓN DE LA CLASE 
+        #endregion
 
+        #region MIEMBROS | CAMPOS | ATRIBUTOS PRIVADOS
+        #endregion
+
+        #region CONSTRUCTORES - MÉTODOS DE INSTANCIACIÓN
+        #endregion
+
+        // Mecanismo de Acceso a la información que mantiene la clase (datos)
+        // Proporciona  dos descriptores de accesso (get y set)
+        // Su mision es la de proteger los datos que entran a la clase (set) 
+        // o se transfieren fuera de la clase (get)
+        #region  PROPIEDADES
+        #endregion
+
+        #region METODOS PRIVADOS - INTERNOS DE LA CLASE 
+        #endregion
+
+        #region METODOS PÚBLICOS - ACCESIBLES DESDE FUERA DE LA CLASE
+        #endregion
     }
 }
